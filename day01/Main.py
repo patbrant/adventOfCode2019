684bc4bf4120b1ea881dbcc3d8d67f42d298b592
wabrants <patrizio.brantschen@stud.hslu.ch>
2019-12-01 19:43:17 +0100

    Add result of day 01

---
+++ day01/Main.py
@@ -0,0 +1,22 @@
+import math
+
+class Main:
+    inputPath = "day01/puzzle01/input.txt"
+
+    def calculateFuelSum(self):
+        modules = self.readModules()
+        resultSum = sum(map(self.calculateFuel, modules))
+        return resultSum
+
+    def readModules(self):
+        moduleFile = open(self.inputPath, "r")
+        moduleList = moduleFile.read().splitlines()
+        return map(int, moduleList)
+
+    def calculateFuel(self, mass):
+        fuel = math.floor(mass / 3) - 2
+        if fuel <= 0:
+            return 0
+        return fuel + self.calculateFuel(fuel)
+
+print(Main().calculateFuelSum())
